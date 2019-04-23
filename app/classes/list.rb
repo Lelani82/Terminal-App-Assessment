e97f5a47d065ee34259fb9d26f7056c65db70a88
lelani82 <lelaniparker@hotmail.com>
2019-04-23 14:16:44 +1000

    list.rb is running correctly, trying to implement app.rb next

--- app/classes/list.rb
+++ app/classes/list.rb
@@ -2,7 +2,7 @@ class List
     attr_accessor :item, :quantity
     def initialize(item, quantity)
         @item = item
-        @quantity = quantity
+        @quantity = quantity.to_i
         @list = Hash.new(0)
     end
 
@@ -20,5 +20,9 @@ class List
     end
 end
 
-ordered_list = List.new(item, quantity)
-ordered_list.add_item(item, quantity).preview
+puts "Add item"
+@item = gets.strip
+puts "Qty"
+@quantity = gets.strip
+ordered_list = List.new(@item, @quantity)
+puts ordered_list.add_item(@item, @quantity).preview
