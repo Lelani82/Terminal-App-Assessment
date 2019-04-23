a999e130c958e44df80749c44ba9b05982ac1ddf
lelani82 <lelaniparker@hotmail.com>
2019-04-23 14:27:25 +1000

    fixed hash bug

--- app/classes/list.rb
+++ app/classes/list.rb
@@ -20,9 +20,33 @@ class List
     end
 end
 
-puts "Add item"
-@item = gets.strip
-puts "Qty"
-@quantity = gets.strip
-ordered_list = List.new(@item, @quantity)
-puts ordered_list.add_item(@item, @quantity).preview
+# puts "Add item"
+# @item = gets.strip
+# puts "Qty"
+# @quantity = gets.strip
+
+#puts ordered_list.add_item(@item, @quantity).preview
+
+loop do 
+    # Ask customer for input
+        puts "Press (a)dd to add an item, or \nPress (p)review to view your current list, or \nPress (q)uit to exit once finished"
+    answer = gets.strip.downcase
+
+    case answer
+    when "a"    # When input is (a)         
+        puts "What item would you like to add to your shopping list?"
+        @item = gets.strip.downcase
+        puts "How many #{@item}'s would you like?"
+        @quantity = gets.strip.to_i
+    when "p"    # When input is (p)
+        ordered_list = List.new(@item, @quantity)
+        puts ordered_list.add_item(@item, @quantity).preview
+    when "q"    # When input is (q)
+        puts "Here's your shopping list:"
+        ordered_list = List.new(@item, @quantity)
+        puts ordered_list.add_item(@item, @quantity).preview
+        break
+    else        # When input is none of the above
+        puts "Invalid option"
+    end
+end
