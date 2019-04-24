d27e8c652cb743674cb12980569f5503c2e19372
lelani82 <lelaniparker@hotmail.com>
2019-04-24 11:53:54 +1000

    working on final shopping list array/ loop

--- app/classes/list.rb
+++ app/classes/list.rb
@@ -23,15 +23,15 @@ class List
         puts "=============================="
     end
 
-    def final                                                   # This creates the final shopping list in categories
-        puts "=============================="
-        puts "Your current shopping list is:"
-        puts "=============================="
-        @list.each do |item, quantity|
-            puts "#{quantity} - #{item.item_name}"              
-        end
-        puts "=============================="
-    end
+    # def final                                                   # This creates the final shopping list in categories
+    #     puts "=============================="
+    #     puts "Your Final Shopping List:"
+    #     puts "=============================="
+    #     @category.each do |item_name, category|
+    #         puts "#{category} - #{item.item_name}"              
+    #     end
+    #     puts "=============================="
+    # end
 
 end
 
@@ -42,13 +42,15 @@ ordered_list = List.new
 loop do 
     puts "Press (a)dd to add an item, or \nPress (p)review to view your current list, or \nPress (q)uit to exit once finished"
     answer = gets.strip.downcase
+    
+    category = %w(fruit/veggies chilled/frozen meat/poultry bakery other)       # Categories array
+        
     case answer
     when "a"                                            # When input is (a)
         puts "What item would you like to add to your shopping list?"
         item_name = gets.strip.downcase
         prompt = TTY::Prompt.new
-        category = %w(fruit/veggies chilled/frozen meat/poultry bakery other)
-        prompt.multi_select('Choose your favourite drink?', category, help: '(Use arrow keys and Enter to finish)')
+        prompt.multi_select('Choose your category', category, help: '(Use arrow keys and Enter to finish)')
         item = Item.new(item_name, category)
         puts "How many #{item_name}'s would you like?"
         quantity = gets.strip.to_i
@@ -56,8 +58,15 @@ loop do
     when "p"                                            # When input is (p)
         puts ordered_list.preview
     when "q"                                            # When input is (q)
-        puts "Here's your shopping list:"
-        puts ordered_list
+        #puts ordered_list.final
+        # This creates the final shopping list in categories
+             puts "=============================="
+             puts "Your Final Shopping List:"
+             puts "=============================="
+             category.each do |item_name, category|
+             puts "#{category} - #{item_name}"              
+             end
+             puts "=============================="        
         break
     else                                                # When input is none of the above
         puts "Invalid option"
